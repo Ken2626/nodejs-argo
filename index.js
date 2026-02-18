a8f67c7e8da18e881f2951fd89c8a86a03161205
Ken2626 <152459912+Ken2626@users.noreply.github.com>
2026-02-18 14:01:32 +0800

    Update ARGO_DOMAIN and ARGO_AUTH defaults

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const UUID = process.env.UUID || '8f38fab9-a554-4985-add5-247b5c027d10'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'channel.010309.xyz';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjE1YThkM2RhMjgzZDdiOTlhODJjZTE0OTlhMWNkOGUiLCJ0IjoiYjY0NzBhNGEtZTYyYi00NmVkLWEzMWQtMzNiN2I5NzAyZjE5IiwicyI6Ik5ESXlNbU0zWlRrdFlXTTRPQzAwT1dZMUxUZzVPR1V0T0RjM1pqVmhZVGxoWVRreSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'saas.sin.fan';            // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'northflank';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
